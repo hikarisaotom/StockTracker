da748ece3800318d05eecad767226f242363b82f
hikarisaotom <claudia.cortes@helloiconic.com>
2024-03-13 03:58:17 -0600

    chart v 1.1

--- src/ui/components/atoms/Chart/Chart.tsx
+++ src/ui/components/atoms/Chart/Chart.tsx
@@ -3,7 +3,7 @@ import {Dimensions, Text} from 'react-native';
 import {LineChart} from 'react-native-chart-kit';
 import {WatchListItem} from '../../../../data/store/types/types';
 
-const Chart = (item: WatchListItem) => {
+const Chart = (item: WatchListItem ) => {
   const chartConfig = {
     backgroundGradientFrom: '#fff',
     backgroundGradientTo: '#fff',
@@ -13,44 +13,10 @@ const Chart = (item: WatchListItem) => {
     barPercentage: 0.5,
     useShadowColorFromDataset: false,
   };
-  // const getValidData = (data) => {
-  //   if (!data) {
-  //     return {labels: [], datasets: []};
-  //   }
-  //   if (!Array.isArray(data.history) || data.history.length === 0) {
-  //     return {labels: [], datasets: []};
-  //   }
-  //   const validData = data.history.filter(item => {
-  //     if (!item.hasOwnProperty('t') || !item.hasOwnProperty('p')) {
-  //       return false;
-  //     }
-  //     if (isNaN(item.t) || isNaN(item.p)) {
-  //       return false;
-  //     }
-  //     return true;
-  //   });
 
-  //   const labels = validData.map((item, index) =>
-  //     index % 2 === 0 ? item.t : '',
-  //   );
-  //   const datasets = [
-  //     {
-  //       data: validData.map(item => item.p),
-  //       color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
-  //       strokeWidth: 2,
-  //     },
-  //   ];
-
-  //   return {
-  //     labels,
-  //     datasets,
-  //     legend: ['Historical Prices for ' + item?.item?.symbol ?? ''],
-  //   };
-  // };
-
-  const getValidData = (data) => {
+  const getValidData = (data: WatchListItem|undefined) => {
     if (!data || !data.history) {
-      return { labels: [], datasets: [] };
+      return {labels: [], datasets: []};
     }
     const validData = data.history.filter(item => {
       if (!item.hasOwnProperty('t') || !item.hasOwnProperty('p')) {
@@ -61,40 +27,55 @@ const Chart = (item: WatchListItem) => {
       }
       return true;
     });
-  
-    const labels = validData.map((item, index) =>
-      index % 2 === 0 ? item.t : '',
-    );
+
+    const labels = validData.map(item => convertNumbers(item.p ?? 0));
     const datasets = [
       {
-        data: validData.map(item => item.p),
+        data: validData.map((item, index) => (index % 2 === 0 ? item.p : '')),
         color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
         strokeWidth: 2,
       },
     ];
-  
+
     return {
       labels,
       datasets,
       legend: ['Historical Prices for ' + (item?.item?.symbol ?? '')],
     };
   };
-  
+  function convertNumbers(number) {
+    const suffixes = ['', 'K', 'M', 'B'];
+    function formatNumber(n) {
+      const formatNumber = new Intl.NumberFormat('en-US');
+     return formatNumber.format(n);
+    }
 
+
+    function getScale(n) {
+      return Math.floor(Math.log10(n) / 3);
+    }
+
+    function convert(number) {
+      const scale = getScale(number);
+      const suffix = suffixes[scale];
+      const formattedValue = formatNumber(number / Math.pow(1000, scale));
+      return formattedValue + suffix;
+    }
+
+    return convert(number);
+}
   return (
     <>
-      {item?.item ? (
+      {item?.item?.history ? (
         <LineChart
           data={getValidData(item.item)}
           width={Dimensions.get('window').width}
-          height={220}
+          height={300}
           chartConfig={chartConfig}
           bezier
           style={{borderRadius: 16}}
-          yAxisLabel={'price'}
-          xAxisLabel={'time'}
-          verticalLabelRotation={45}
-          horizontalLabelRotation={45}
+          verticalLabelRotation={20}
+          horizontalLabelRotation={20}
         />
       ) : (
         <Text style={{textAlign: 'center', fontSize: 20, marginBottom: 10}}>
